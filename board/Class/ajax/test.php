400230025e10db6e69b0cbb62fd07d10c071d0d0
Gen Taguchi <kunimi@dotinstall.com>
2015-05-29 02:10:12 +0900

    modal ajax updated

--- board/Class/ajax/test.php
+++ board/Class/ajax/test.php
@@ -2,7 +2,7 @@
 if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
 	$a = $_POST['text'];
 	header("Content-Type: text/plain; charset=UTF-8");
-
+  print('いらっしゃいませ！');
 	exit;
 }
 
--- board/Class/modal.js
+++ board/Class/modal.js
@@ -4,6 +4,21 @@ $(function () {
 autoOpen: true,  // 自動でオープンしない
 modal: true,      // モーダル表示する
 });
+$(function() { //#formがsubmitされた時
+								var form  = $('#form');
+								$.ajax({
+url : form.attr('action'), //送信先のURL。フォームから取得
+type: form.attr('method'), //送信メソッド。フォームから取得
+data: form.serialize(), //送信するデータ。フォームから取得
+success: function(response) { //通信成功時の処理はここに書く
+document.getElementById("modal").innerHTML = (response);
+},
+error: function(xhr) { //通信失敗時の処理はここに書く
+alert(xhr.responseText);
+}
+});
+								return false; 
+								});
 								$('selecter').click(function () {
 												$('#modal').dialog('open');
 												return false;
