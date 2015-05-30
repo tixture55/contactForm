a3eb769903b2f5eb29410a53f848f329366e0878
Gen Taguchi <kunimi@dotinstall.com>
2015-05-31 07:45:44 +0900

    first chat view

--- board/Class/ajax/ajax.js
+++ board/Class/ajax/ajax.js
@@ -1,5 +1,20 @@
 
 $(function () {
+																$(function() { //#formがsubmitされた時
+																				var form  = $('#form');
+																				$.ajax({
+url : form.attr('action'), //送信先のURL。フォームから取得
+type: form.attr('method'), //送信メソッド。フォームから取得
+data: form.serialize(), //送信するデータ。フォームから取得
+success: function(response) { //通信成功時の処理はここに書く
+document.getElementById("post_com").innerHTML = response;
+},
+error: function(xhr) { //通信失敗時の処理はここに書く
+alert(xhr.responseText);
+}
+});
+																				return false; 
+																				});
 																$(document).on('click', '.ui-widget-overlay', function(){
 																				$(this).prev().find('.ui-dialog-content').dialog('close');
 
