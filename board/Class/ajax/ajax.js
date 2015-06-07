b2be0f75cf1b383d90d5bfa5e3eafb6d9de65aaf
Gen Taguchi <kunimi@dotinstall.com>
2015-06-07 22:12:24 +0900

    apc previous

--- board/Class/ajax/ajax.js
+++ board/Class/ajax/ajax.js
@@ -8,7 +8,6 @@ type: form.attr('method'), //送信メソッド。フォームから取得
 data: form.serialize(), //送信するデータ。フォームから取得
 success: function(response) { //通信成功時の処理はここに書く
 document.getElementById("post_com").innerHTML = response;
-
 },
 error: function(xhr) { //通信失敗時の処理はここに書く
 							 alert(xhr.responseText);
