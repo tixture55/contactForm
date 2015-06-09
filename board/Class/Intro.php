b6b1c08c7cfe02c58d2598b985e93d1784a062c3
Gen Taguchi <kunimi@dotinstall.com>
2015-06-09 18:22:30 +0900

    di previous

--- board/Class/Intro.php
+++ board/Class/Intro.php
@@ -14,8 +14,6 @@ body {
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
 <script type="text/javascript" src="./script.js"></script>
-<script type="text/javascript" src="./cross.js"></script>
-<script type="text/javascript" src="./ajax/ballon.js"></script>
 <script type="text/javascript" src="./ajax/ajax.js"></script>
 
 <script>
