b6338c26fa0c8948f59354f737b3e26b07884104
Gen Taguchi <kunimi@dotinstall.com>
2015-08-08 18:14:21 +0900

    observer_pattern

--- board/Class/ajax/ajax.js
+++ board/Class/ajax/ajax.js
@@ -22,6 +22,7 @@ url : form.attr('action'), //送信先のURL。フォームから取得
 type: form.attr('method'), //送信メソッド。フォームから取得
 data: form.serialize(), //送信するデータ。フォームから取得
 success: function(response) { //通信成功時の処理はここに書く
+alert(response);
 document.getElementById("post_com").innerHTML = response;
 },
 error: function(xhr) { //通信失敗時の処理はここに書く
