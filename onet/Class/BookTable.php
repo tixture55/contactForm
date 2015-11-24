83548311867c6cf34e46498ff392cc41877ff74b
Gen Taguchi <kunimi@dotinstall.com>
2015-11-24 14:40:36 +0900

    TranTable added

--- onet/Class/BookTable.php
+++ onet/Class/BookTable.php
@@ -15,20 +15,30 @@ class BookTable extends ModelBase{
 			$sql->bindParam(':sid',$id);
 			$sql->execute();
 			//access_logテーブルに参照者と参照されたユーザと日時を追加
-			$sql2 = "insert into access_log (id,accessor,be_accessed,ins_date) values(null,".$id.",2,now())";
-			$sql3 = "SELECT * FROM Customer WHERE First_Name = 'sachiko' FOR UPDATE;";
-			$sql4 = "update Customer set hobby ='programming' where First_Name ='sachiko'";
+			$sql2 = "insert into access_log (id,accessor,be_accessed,ins_date) values(null,".$id.",9,now())";
+			$sql3 = "SELECT * FROM Customer WHERE Last_Name = 'sachiko' OR Last_Name ='joe' OR area = 34 FOR UPDATE;";
+			$sql4 = "update Customer set hobby ='progf' where Last_Name ='sachiko'";
+			$sql5 = "update Customer set hobby ='fishing' where Last_Name ='joe'";
+			$sql6 = "update Customer set area ='izuoka' where age = 34";
 			$stmt = $this->db->prepare($sql2);
 			$stmt2 = $this->db->prepare($sql3);
 			$stmt3 = $this->db->prepare($sql4);
+			$stmt4 = $this->db->prepare($sql5);
+			$stmt5 = $this->db->prepare($sql6);
 			$stmt->execute();
-			$stmt2->execute();
-			$stmt3->execute();
+			//$stmt2->execute();
+			for($i =0;$i < 10; $i++){
+				$stmt3->execute();
+			}
+			$stmt4->execute();
+			$stmt5->execute();
 			$stmt->closeCursor();
 			$stmt2->closeCursor();
 			$stmt3->closeCursor();
+			$stmt4->closeCursor();
+			$stmt5->closeCursor();
 
-
+      sleep(7);
 			$this->db->commit();
 			echo 'commit done';
 		}
